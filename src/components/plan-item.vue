a5df0d625b4d2d71ee52b9813403e1e4599bec75
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-04-11 11:47:23 -0600

    ⭐First component

---
+++ src/components/plan-item.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="plan">
+        <div class="description">
+          <span class="title">
+            El Soltero
+          </span>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
