91f0445a09d3914bf072d9bb60c8411ef46acb47
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-04-25 12:11:48 -0600

    ⭐Seleccion de un plan

---
+++ src/components/app-title.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="titles">
+    <div class="description">
+      <span class="title">{{ name }}</span>
+    </div>
+  </h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const title = defineProps({
+  name: { type: String }
+});
+</script>
+
+  
--- src/components/plan-picker-item.vue
+++ src/components/plan-picker-item.vue
@@ -1,14 +1,23 @@
 <template>
-   <div class="plan">
+   <div @click="select" :class="{'selected-plan':selected}"
+   class="plan">
         <div class="description">
           <span class="title">  
-            {{ name }} 
+            {{ name }} {{ selected ? "✅" :"" }}
           </span>
         </div>
       </div>
 </template>
 
+
 <script setup>
+import {ref} from 'vue';
+//Definir una variable, permitiendo a cada item saber si a sido seleccionada o no
+const selected = ref(false);
+//metodo para cambiar a true
+const select = ()=> {
+  selected.value=true
+}
 //Macro es una funcion especialidad en vue que me permite crear defineProps (que ya las importa), no se tiene que intregrar porque Vue ya lo hace, para agregar  nombre propiedades siempre usar camelcasing
 defineProps({
   name: {type: String, 
@@ -18,4 +27,11 @@ defineProps({
 //Esquema de propiedades de manera mas avanzada (objeto), las propiedades van dentro de las llaves {} 
 //con el objetivo de proveer un objeto, no un arreglo
 </script>
+
+<style scoped>
+.selected-plan{
+  background-color:darkkhaki;
+}
+</style>
+
 
