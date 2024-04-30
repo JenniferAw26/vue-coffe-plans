4655e4d4790b378178c05b82e91413cf44ef7734
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-04-30 12:55:04 -0600

    🍱Slots

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+ 
+<template>
+<div class="content">
+  <button> Haz clic 👆</button>
+</div>
+</template>
+ 
+<style scoped>
+.content{
+  text-align: center;
+}
+button{
+  width: 10rem;
+  margin: 30px auto;
+}
+</style>
